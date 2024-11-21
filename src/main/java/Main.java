ee504a29f241c4657c5316205715a8586a1d26a6
Ruan Vitor Moreno de Oliveira <ruan.moreno@pjus.com.br>
2024-11-21 20:52:44 -0300

    Testando workflow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, CI/CD with GitHub Actions!");
 
-        System.out.println("Sorted Array: " + Arrays.toString(sortArray(new int[]{5, 3, 1, 8, 1, 2})));
+        System.out.println("Sorted Arrayy: " + Arrays.toString(sortArray(new int[]{5, 3, 1, 8, 1, 2})));
     }
 
     public static int[] sortArray(int[] numbers) {
